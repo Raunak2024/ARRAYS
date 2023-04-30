cac50afbeea34e16732f42081389c2110078532c
Raunak Nag <85424111+Raunak2024@users.noreply.github.com>
2023-04-30 13:07:37 +0530

    Add files via upload

---
+++ MoveZeroArrayEnd.cpp
@@ -0,0 +1,12 @@
+//Move Zeroes to Array End Variations- Move negatives to end and positive to start
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    // Brute  Approach-1) Creating an auxillary temporary array and storing the non-zero elements inside it and then overwriting the original array and keeping only the remaining array end filled with zeroes TC-O(N),SC-O(N)
+    for(i=0;i<10;i++){
+
+}
+// Best Approach-2) Swapping using Two Pointers TC-O(N) SC-O(1)
+
+}
