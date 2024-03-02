f25ace4a7e4c3e4cf0013f638b9554c9f37e7a40
Raunak Nag <85424111+Raunak2024@users.noreply.github.com>
2024-03-02 22:38:37 +0530

    Update REVERSEARRAY.cpp

--- REVERSEARRAY.cpp
+++ REVERSEARRAY.cpp
@@ -1,3 +1,105 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+//Input - {4, 5, 1, 2}
+//Output - {2, 1, 5, 4}
+
+//Input - {4, 5, 1, 0, 2}
+//Output - {2, 0, 1, 5, 4}
+
+//Input - {3}
+//Output - {3}
+
+//INTUITION
+//I/P      O/P
+//0TH IDX  N-1TH IDX
+//1ST IDX  N-2TH IDX
+//2ND IDX  N-3RD IDX
+//3RD IDX  N-4TH IDX
+
+
+//APPROACH 1 - EXTRA ARRAY
+
+void rev_arr(int arr[], int size )
+{
+    //DEC EXT ARR OF ORG ARR SIZE
+    int ext_arr[size];
+
+    //COPY ALL ORG ARR ELES INTO EXT ARR IN REVERSE ORDER
+    for(int i = 0; i < size; i++)
+    {
+        ext_arr[i] = arr[size - 1 - i];
+    }
+
+    //COPY BACK ALL REV ARR ELS BACK INTO ORG ARR
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = ext_arr[i];
+    }
+    
+    //FINALLY PRINT THE ORG ARR
+    for(int i = 0; i < size; i++)
+    {
+        cout<< arr[i];
+    }
+
+    //TC - O(N) SC-O(N)
+
+//APPROACH 2 STACK - LIFO ORDER
+
+stack<int>stack;//decl stack of int type
+
+// Push each ele of the org arr into a stack
+for(int i = 0; i < size; i++)
+{
+    stack.push(arr[i]);
+}
+
+// Pop ele from the stack to form the rev arr and print it
+for(int i = 0; i < size; i++)
+{
+   arr[i] = stack.top();
+   stack.pop();
+   cout << arr[i];
+}
+
+// TC-O(N) SC-O(N)
+
+//APPROACH 3 TWO POINTERS - SWAPPING
+int left = 0, right = n - 1;
+
+//SWAP WITH 2 POINTERS FROM START AND END
+    while(left <= right) //< : EVEN = : ODD
+    {
+    swap(arr[left], arr[right]);
+    //int temp = arr[left]; 
+    //arr[left] = arr[right]; 
+    //arr[right] = temp; 
+    left++;
+    right--;
+    }
+
+//FINALLY PRINT THE ORG ARR
+    for(int i = 0; i < size; i++)
+    {
+        cout<< arr[i];
+    }
+    //TC - O(N), SC - O(1)
+
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    rev_arr(arr, size);
+}
+
+
+
+
 // Reverse the array using minimum comparisons-
 //The entire logic for reversing a array/string is based on using the opposite directional two-pointer approach!
 #include<iostream>
